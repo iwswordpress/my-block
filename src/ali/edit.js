4dcb1a07e6e4b313b8854b3b4c904224028c9e29
iwswordpress <iwswordpress@gmail.com>
2020-08-09 11:34:58 +0100

    notsure

--- src/ali/edit.js
+++ src/ali/edit.js
@@ -6,7 +6,7 @@ const {
 	InspectorControls,
 	PanelColorSettings,
 } = wp.blockEditor;
-
+const { RangeControl, PanelBody } = wp.components;
 const { Fragment, Component } = wp.element;
 
 class Edit extends Component {
@@ -25,12 +25,48 @@ class Edit extends Component {
 	onChangeTextColor = (textColor) => {
 		this.props.setAttributes({ textColor });
 	};
+	onToggleShadow = () => {
+		this.props.setAttributes({ shadow: !this.props.attributes.shadow });
+		console.log("SHADOW NOW: ", this.props.attributes.shadow);
+	};
+	onChangeShadowOpacity = (shadowOpacity) => {
+		this.props.setAttributes({ shadowOpacity });
+	};
+
 	render() {
 		const { className, attributes } = this.props;
-		const { content, alignment, backgroundColor, textColor } = attributes;
+
+		const {
+			content,
+			alignment,
+			backgroundColor,
+			textColor,
+			shadow,
+			shadowOpacity,
+		} = attributes;
+		let myStyles = "";
+		let myBoxShadow = "";
+		this.props.attributes.shadow
+			? (myStyles = "7px dashed red")
+			: (myStyles = "");
+		this.props.attributes.shadow
+			? (myBoxShadow = "10px 30px 20px rgba(0,0,0," + shadowOpacity)
+			: (myBoxShadow = "");
 		return (
 			<Fragment>
 				<InspectorControls>
+					<PanelBody title={__("OPACITY", "mytheme-blocks")}>
+						{shadow && (
+							<RangeControl
+								label={__("Shadow Opacity", "mytheme-blocks")}
+								value={shadowOpacity}
+								onChange={this.onChangeShadowOpacity}
+								min={0.2}
+								max={0.8}
+								step={0.05}
+							/>
+						)}
+					</PanelBody>
 					<PanelColorSettings
 						title={__("Panel", "mytheme-blocks")}
 						colorSettings={[
@@ -47,7 +83,18 @@ class Edit extends Component {
 						]}
 					/>
 				</InspectorControls>
-				<BlockControls>
+				<BlockControls
+					controls={[
+						[
+							{
+								icon: "admin-settings",
+								title: __("SHADOW", "mytheme-blocks"),
+								onClick: this.onToggleShadow,
+								isActive: shadow,
+							},
+						],
+					]}
+				>
 					<AlignmentToolbar
 						value={alignment}
 						onChange={this.onChangeAlignment}
@@ -63,6 +110,8 @@ class Edit extends Component {
 						textAlign: alignment,
 						backgroundColor: backgroundColor,
 						color: textColor,
+						border: myStyles,
+						boxShadow: myBoxShadow,
 					}}
 				/>
 			</Fragment>
